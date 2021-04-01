e957d6e7fa384393da928e5f53a2d87b4b42ba5a
Leaf-life <77422322+Leaf-life@users.noreply.github.com>
2021-03-31 19:50:03 -0600

    added StateMachine to driveToDistance and cleaned up the code

--- components/driveToDistance.py
+++ components/driveToDistance.py
@@ -1,19 +1,34 @@
-class DistanceDriver(StateMachine):
+from magicbot import StateMachine, state, timed_state, tunable, feedback
+from components.lidar import getDist
 
-    def driveDistance(lidar.getDist):
-        if lidar.getDist == 0:
-            print("Distance is invaled")
+
+class driverToDistance(StateMachine):
+    @state
+    def idling(self):
+        if self.start:
+            self.next_state('setInitialPosition')
+
+    @state
+    def setInitialPosition(self):
+        self.initialPosition = getDist
+        self.next_state('StartDrive')
+
+    @state
+    def StartDrive(self):
+        if getDist == 0:
+            print("Distance is invalid")
+            self.next_state('idling')
         else:
-            potisioni = lidar.getDist
-            distanceToTravel = potisioni - distanceSet
-            return distanceToTravel
+            self.driveDistance = self.initialPosition - self.distanceSet
+            self.next_state('drive')
 
+    @state
+    def drive(self):
+        DriveTrain.setTank
+        if self.driveDistance >= getDist:
+            self.driveDistance = 0
+            self.next_state('idling')
+            print("You have reached destnation")
 
-    def setDistance():
-        distanceSet = 30
-        return distanceSet
-
-
-    def TravelDistance(n):
-        if (n == lidar.getDist):
-            print("You have reched destnation")
+    def setDistance(self, distanceSet):
+        self.distanceSet = distanceSet
