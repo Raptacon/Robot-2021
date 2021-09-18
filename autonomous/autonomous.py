ed3c44fac2710c18d78f83c7fd7bf45da23b2baa
ManiacMoustache <cjkesler@unionvotes.net>
2021-09-18 15:58:38 -0600

    Added loader-stopping logic to autonomous shooting and added a demo for shooting in autonomous

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -2,6 +2,9 @@ from magicbot import AutonomousStateMachine, tunable, timed_state, state
 from components.driveTrain import DriveTrain
 from components.shooterLogic import ShooterLogic
 from components.shooterMotors import ShooterMotorCreation
+from components.autoAlign import AutoAlign
+from components.autoShoot import AutoShoot
+from components.shooterLogic import ShooterLogic
 from components.pneumatics import Pneumatics
 
 class Autonomous(AutonomousStateMachine):
@@ -13,33 +16,16 @@ class Autonomous(AutonomousStateMachine):
     shooter: ShooterLogic
     shooterMotors: ShooterMotorCreation
     pneumatics: Pneumatics
+    autoAlign: AutoAlign
+    autoShoot: AutoShoot
+    shooter: ShooterLogic
     drive_speed = tunable(.25)
 
     @state(first = True)
     def engage_shooter(self):
         """Starts shooter and fires"""
-        self.pneumatics.deployLoader()
-        self.shooter.shootBalls()
-        self.next_state('shooter_wait')
-
-    @state
-    def shooter_wait(self):
-        """Waits for shooter to finish, then next state"""
-        if self.shooter.current_state == 'idling':
-            self.next_state_now('drive_backwards')
-
-    @timed_state(duration = time, next_state = 'turn')
-    def drive_backwards(self):
-        """Drives the bot backwards for a time"""
-        self.driveTrain.setTank(self.drive_speed, self.drive_speed)
-
-    @timed_state(duration = time, next_state = 'stop')
-    def turn(self):
-        """Turns for a time"""
-        self.driveTrain.setTank(-self.drive_speed, self.drive_speed)
-
-    @state(must_finish = True)
-    def stop(self):
-        """Stops driving bot"""
-        self.driveTrain.setTank(0, 0)
-        self.done()
+        self.autoAlign.setShootAfterComplete(True)
+        self.autoAlign.engage()
+        self.autoShoot.engage()
+        self.shooter.engage()
+        self.next_state("engage_shooter")
--- components/shooterLogic.py
+++ components/shooterLogic.py
@@ -117,7 +117,12 @@ class ShooterLogic(StateMachine):
     @timed_state(duration = shooterStoppingDelay, next_state = 'finishShooting')
     def autonomousShoot(self):
         """Shoot balls when shooter is up to speed. Strictly for autonomous use."""
-        self.shooterMotors.runLoader(self.shootingLoaderSpeed, Direction.kForwards)
+        if self.isShooterUpToSpeed():
+            log.error("Shoottinginoiansdonosg")
+            self.shooterMotors.runLoader(self.shootingLoaderSpeed, Direction.kForwards)
+        else:
+            self.shooterMotors.runLoader(0, Direction.kForwards)
+            self.next_state('autonomousShoot')
 
     @state
     def finishShooting(self):
