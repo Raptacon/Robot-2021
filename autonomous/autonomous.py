f4d620f1fc6f0b01601763e6182d696a2efea495
ManiacMoustache <cjkesler@unionvotes.net>
2021-09-18 16:02:52 -0600

    Demo for goToDist in autonomous

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -3,6 +3,7 @@ from components.driveTrain import DriveTrain
 from components.shooterLogic import ShooterLogic
 from components.shooterMotors import ShooterMotorCreation
 from components.pneumatics import Pneumatics
+from components.driveTrainGoToDist import GoToDist
 
 class Autonomous(AutonomousStateMachine):
     """Creates the autonomous code"""
@@ -13,33 +14,11 @@ class Autonomous(AutonomousStateMachine):
     shooter: ShooterLogic
     shooterMotors: ShooterMotorCreation
     pneumatics: Pneumatics
+    goToDist: GoToDist
     drive_speed = tunable(.25)
 
     @state(first = True)
     def engage_shooter(self):
         """Starts shooter and fires"""
-        self.pneumatics.deployLoader()
-        self.shooter.shootBalls()
-        self.next_state('shooter_wait')
-
-    @state
-    def shooter_wait(self):
-        """Waits for shooter to finish, then next state"""
-        if self.shooter.current_state == 'idling':
-            self.next_state_now('drive_backwards')
-
-    @timed_state(duration = time, next_state = 'turn')
-    def drive_backwards(self):
-        """Drives the bot backwards for a time"""
-        self.driveTrain.setTank(self.drive_speed, self.drive_speed)
-
-    @timed_state(duration = time, next_state = 'stop')
-    def turn(self):
-        """Turns for a time"""
-        self.driveTrain.setTank(-self.drive_speed, self.drive_speed)
-
-    @state(must_finish = True)
-    def stop(self):
-        """Stops driving bot"""
-        self.driveTrain.setTank(0, 0)
-        self.done()
+        self.goToDist.setTargetDist(60)
+        self.goToDist.start()
